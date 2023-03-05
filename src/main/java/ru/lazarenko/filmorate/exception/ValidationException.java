22b7f7201bd7f57f792f69da19c11a8cd68a49b5
Anastasia <an.afalin@yandex.ru>
2023-03-06 00:09:33 +0300

    added user and film controllers

---
+++ src/main/java/ru/lazarenko/filmorate/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package ru.lazarenko.filmorate.exception;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
